eaf2656280ccd9e63da4c8f159eaaa923ed9c702
josemwas1984 <jose.maina79@gmail.com>
2020-02-24 10:29:27 +0300

    added img descriptions

--- scripts.js
+++ scripts.js
@@ -2,18 +2,15 @@ $('.main').click(function () {
 	$(this).children('.hide').slideToggle(1200);
 	$(this).children('.show').toggle(1500);
   });
-  // giving feedback to the customer
-  $("button").click(function (omonge) {
+  $("button").click(function (any) {
 	var client = document.getElementById('nameDetail').value;
-	alert(' Dear ' + ' esteemed ' + client + ' we have received your message. ' + ' Feel free to reach us anytime');
-	omonge.preventDefault();
+	alert(' Dear ' + ' esteemed ' + name + ' we have received your message. ' + ' Feel free to reach us anytime');
+	name.preventDefault();
   });
-  //    reset my form button
   $("button").on('click', function () {
 	$('form').each(function () {
 	  this.reset();
 	});});
-  //portifolio section 
   $(".card").hover(function () {
 	$(this).children(".card-1").fadeToggle(1000, "linear");
   });
